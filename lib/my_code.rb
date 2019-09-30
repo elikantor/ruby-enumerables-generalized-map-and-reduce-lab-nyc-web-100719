a5efa0fa8e63736b4dcb580f1620e503ae247037
Eli Kantor <eli.kantor@outlook.com>
2019-09-30 01:27:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -22,10 +22,7 @@ def reduce(source_array, starting_point = 0)
   while i < source_array.length 
 
     yield(source_array[i], result)
-
     result += source_array[i]
-
-    
     i += 1 
   end
   
