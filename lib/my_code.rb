077e169df11b215c86d0a36dea6dd0ef555277fc
Eli Kantor <eli.kantor@outlook.com>
2019-09-30 00:52:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,3 +14,18 @@ def map(array)
 end
 
 
+def reduce(source_array, starting_point = 0)
+  
+  i = 0 
+  result = starting_point
+  
+  while i < source_array.length 
+    yield result += source_array[i]
+    
+    i += 1 
+  end
+  
+  return result
+
+end
+
